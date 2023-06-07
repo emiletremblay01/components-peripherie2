912ec4b5d8408e28fe25923482ee7f44436a53f7
Emile Tremblay <emiletremblay01@gmail.com>
2023-06-07 14:23:51 -0400

    removed Transition import

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Disclosure, Transition } from "@headlessui/react";
+import { Disclosure } from "@headlessui/react";
 import { villeAvecPointsDeVente } from "../types/types";
 const villesAvecPointsDeVente: villeAvecPointsDeVente[] = [
   {
